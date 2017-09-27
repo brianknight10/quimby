63097d88a303c4e48c7dc5fe28ca18856c007008
Brian Knight <brianknight10@gmail.com>
2017-09-27 11:18:45 -0400

    Remove boilerplate rspec mock configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,12 +30,12 @@ RSpec.configure do |config|
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
+  # config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
+    # mocks.verify_partial_doubles = true
+  # end
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
